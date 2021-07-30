b21ba82b8ebb102c171e24eeb514244040034bda
msenat7432 <maxsenat7432@gmail.com>
2021-07-30 10:21:27 -0400

    new scripts

--- scripts-file/Hello.sh
+++ scripts-file/Hello.sh
@@ -7,3 +7,26 @@ echo "the Movies are : ${names[0]} ${names[1]}"
 echo "enter movies "
 read 
 echo "movies : $REPLY"
+
+read -p 'Enter number:' number 
+if [ $((number%2)) -eq 0 ]
+then 
+    echo "even"
+else 
+    echo "odd"
+fi
+
+read -p 'Enter mark:' marks
+if [ "$marks" -lt 40 ]
+then
+    echo "F"
+elif [ "$marks" -gt 41 ] && [ "$marks" -lt 50 ]
+then 
+    echo "D"
+elif [ "$marks" -gt 51 ] && [ "$marks" -lt 70 ]
+then 
+    echo "B"
+elif [ "$marks" -gt 70 ] 
+then 
+    echo "A"
+fi 
